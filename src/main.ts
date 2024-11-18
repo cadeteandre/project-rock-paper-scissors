d0fb1ce4633807ddddf3b03761a09ddebc714e94
Andre Cadete <andrecadete70@gmail.com>
2024-11-18 14:16:32 +0100

    deployment

--- src/main.ts
+++ src/main.ts
@@ -59,12 +59,12 @@ function runningGame(playerMove: string, computerMove: string): void {
   if(roundsCount === roundsAmount) {
     if(playerWinsCount > computerWinsCount) {
       letsPlayDiv.innerHTML = `
-      <div class="finally__img"><img src="./src/images/you-win.png"></div>
+      <div class="finally__img"><img src="../public/you-win.png"></div>
     `;
     } else if(computerWinsCount > playerWinsCount) {
       letsPlayDiv.innerHTML = `
-      <div class="finally__img"><img src="./src/images/game-over-2.png"></div>
-      <div class="finally__img"><img src="./src/images/game-over.png"></div>
+      <div class="finally__img"><img src="../public/game-over-2.png"></div>
+      <div class="finally__img"><img src="../public/game-over.png"></div>
     `;
     }
   }

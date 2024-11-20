b2579320a2a0012b13119597941d7c9c93d49a03
Andre Cadete <andrecadete70@gmail.com>
2024-11-20 16:50:16 +0100

    img problem

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './style.css'
-import * as winLogo from './images/you-win.png';
 
 //* ---------------------- Selecting HTML Elements ----------------------
 const roundsOptions = document.querySelectorAll('.roundsCount') as NodeListOf<HTMLInputElement>;
@@ -60,12 +59,12 @@ function runningGame(playerMove: string, computerMove: string): void {
   if(roundsCount === roundsAmount) {
     if(playerWinsCount > computerWinsCount) {
       letsPlayDiv.innerHTML = `
-      <div class="finally__img"><img src={${winLogo}}></div>
+      <div class="finally__img winLogo"></div>
     `;
     } else if(computerWinsCount > playerWinsCount) {
       letsPlayDiv.innerHTML = `
-      <div class="finally__img"><img src={${winLogo}}></div>
-      <div class="finally__img"><img src={${winLogo}}></div>
+      <div class="finally__img winLogo"></div>
+      <div class="finally__img winLogo"></div>
     `;
     } else if(computerWinsCount === playerWinsCount) {
       letsPlayDiv.innerHTML = `
